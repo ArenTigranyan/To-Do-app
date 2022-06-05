05c1ccf40011eab4572e71652ddb01373e4e9815
ArenTigranyan <tigranyanaren12@gmail.com>
2022-06-05 19:43:50 +0400

    added list

---
+++ main.js
@@ -0,0 +1,32 @@
+const add = document.getElementById('add');
+const list = document.getElementById('list');
+let indexOfToDo = 0;
+
+const addToDo = () => {
+  const title = document.getElementById('input').value;
+  if (title !== '') {
+    indexOfToDo++;
+    let todo = document.createElement('div');
+    let p = document.createElement('p');
+    let deletebtn = document.createElement('button');
+    deletebtn.className = 'delete';
+    deletebtn.innerHTML = 'X';
+    todo.className = 'todo'
+    p.className = 'title';
+    p.append(title);
+    todo.append(p, deletebtn);
+    list.append(todo);
+    p.onclick = () => {
+      p.classList.toggle('checked');
+    };
+    deletebtn.onclick = () => {
+      todo.style.display = 'none';
+    };
+    console.log(p.classList);
+    document.getElementById('input').value = '';
+  } else {
+    alert('Type something');
+  }
+};
+
+add.addEventListener('click', addToDo);
